b934885f779523b5a9e4d4f6d2abaa34f34671e5
samandeep007 <115439036+samandeep007@users.noreply.github.com>
2024-07-30 18:49:12 -0600

    chore: Promisify loadScript function for better code organization and error handling

--- promisification/script.js
+++ promisification/script.js
@@ -1,5 +1,16 @@
 // Promisification
 
+function promisify(fn) {
+    return function(...args){
+        return new Promise((resolve, reject) => {
+            fn(...args, (error, result) => {
+                if(error) return reject(error);
+                return resolve(result)
+            })
+        });
+    }
+}
+
 // I want to create a function that can load any script
 function loadScript(src, callback) {
     const script = document.createElement('script');
@@ -10,12 +21,13 @@ function loadScript(src, callback) {
     
 }
 
+const loadScriptNew = promisify(loadScript);
+loadScriptNew('test.js').then(() => console.log("done")).catch(error => console.log("Error ho gya"))
 
-
-loadScript("test.js", (err, script) => {
-    if(err){
-        console.log(err)
-    }
-    else{
-        console.log("Script loaded");
-    }});
+// loadScript("test.js", (err, script) => {
+//     if(err){
+//         console.log(err)
+//     }
+//     else{
+//         console.log("Script loaded");
+//     }});
